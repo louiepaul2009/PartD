c81ab7e5e3f5aa4d4205e0f731522a10ad89070f
louie <louiepaul2009@gmail.com>
2020-05-19 19:22:42 +1200

    4th commit

--- PartD/src/Main.java
+++ PartD/src/Main.java
@@ -3,10 +3,10 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hi, my name is Louie");
-		System.out.println("My favourite colour is blue");
-		System.out.println("My favourite mode of transportation is by car");
-		
+		System.out.println("Hi, my name is Louie.");
+		System.out.println("My favourite colour is blue.");
+		System.out.println("My favourite mode of transportation is by car.");
+		System.out.println("The best day of the week is Saturday.");
 	}
 
 }
